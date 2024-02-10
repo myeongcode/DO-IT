b09c1e250fe046621c8fb6a305d8d69479f41664
MyeongGyu <woomg427@yonsei.ac.kr>
2024-02-10 21:25:30 +0900

    [Fix] .env.local 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `https://yonsei-do-it.vercel.app/:path*`,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
