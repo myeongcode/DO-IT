c12245128ca63fb9f06b247b499ec4facf1b18be
MyeongGyu <woomg427@yonsei.ac.kr>
2024-02-22 16:47:56 +0900

    [Fix] recruit layout-2

--- src/app/recruit/page.js
+++ src/app/recruit/page.js
@@ -3,19 +3,224 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 import RecruitLanding from '../(components)/(recruit)/RecruitLanding';
 
+const fields = [
+  {
+    id: 0,
+    field: 'Product Manager (PM)',
+    name: '기획자',
+    description:
+      '프로덕트 기획 및 프로젝트가 원활히 진행될 수 있도록 전략수립과 개발자/디자이너와의 소통을 통해 효율적인 프로젝트 관리를 진행합니다.',
+  },
+  {
+    id: 1,
+    field: 'Product Designer (PD)',
+    name: '디자이너',
+    description:
+      '브랜드 아이덴티티 (BI), 사용자 인터페이스 및 경험 (UI/UX) 디자인 등을 통해 프로젝트의 시각화 및 전반적인 사용 경험을 디자인합니다.',
+  },
+  {
+    id: 2,
+    field: 'Front-end Developer (FE)',
+    name: 'CLIENT 개발자',
+    description:
+      '프론트엔드 개발자로 서비스가 실제로 사용자에게 동작되고, SERVER와의 통신을 원활히 할 수 있도록 개발합니다.',
+  },
+  {
+    id: 3,
+    field: 'Back-end Developer (BE)',
+    name: 'SERVER 개발자',
+    description:
+      '백엔드 개발자로 사용자가 요청하는 데이터를 저장하고 처리하며 CLIENT와의 통신을 원활히 할 수 있도록 개발합니다.',
+  },
+];
+
 export default function Recruit() {
+  const [fieldsDetail, setFieldDetail] = useState(false);
+
+  function onClickField(e) {
+    e.preventDefault();
+    setFieldDetail(!fieldsDetail);
+  }
+
   return (
     <div className="flex flex-col w-full items-center overflow-hidden">
       <RecruitLanding />
       <div className="flex justify-center w-full py-40">
         <div className="flex flex-col w-[70%]">
-          <div className="flex flex-col space-y-3">
+          <div className="flex flex-col space-y-3 pb-40 font-suit font-bold">
             <span className="text-6xl text-[#00B8FF]">DO IT.</span>
             <span className="text-6xl">1기 신규부원 모집</span>
           </div>
-          <div>ddd</div>
+          <div className="flex flex-col">
+            <div className="flex w-full justify-between py-20">
+              <span className="text-5xl font-suit">SCHEDULE</span>
+              <div className="flex flex-row w-3/5 justify-between font-pretend">
+                <div className="flex flex-col space-y-3">
+                  <span className="text-5xl">2/27 - 3/3</span>
+                  <span className="text-xl text-[#657786]">서류 접수</span>
+                </div>
+                <div className="flex flex-col space-y-3">
+                  <span className="text-5xl">3/5 - 3/7</span>
+                  <span className="text-xl text-[#657786]">면접 진행</span>
+                </div>
+                <div className="flex flex-col space-y-3">
+                  <span className="text-5xl">3/9</span>
+                  <span className="text-xl text-[#657786]">최종 결과</span>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div className="flex w-full justify-between py-20">
+              <span className="text-5xl font-suit">FIELDS</span>
+              <div className="flex flex-row w-3/5 font-pretend">
+                <div className="flex flex-col w-full">
+                  {fields.map((fieldItem, idx) => {
+                    return (
+                      <div key={idx} className="w-full">
+                        <div className="flex justify-between items-center py-10">
+                          <span className="text-4xl">{fieldItem.field}</span>
+                          <div className="flex flex-row items-center space-x-4">
+                            <span className="text-xl text-[#657786]">
+                              {fieldItem.name}
+                            </span>
+                            <button onClick={onClickField}>
+                              <motion.svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="34"
+                                height="18"
+                                viewBox="0 0 34 18"
+                                fill="none"
+                                className="w-[24px]"
+                                variants={{
+                                  disable: {
+                                    rotate: 0,
+                                  },
+                                  visible: {
+                                    rotate: 180,
+                                  },
+                                }}
+                                transition={{
+                                  duration: 0.3,
+                                }}
+                                animate={!fieldsDetail ? 'disable' : 'visible'}
+                              >
+                                <path d="M1 1L17 16L33 1" stroke="#14171A" />
+                              </motion.svg>
+                            </button>
+                          </div>
+                        </div>
+                        <motion.div
+                          className={`${
+                            fieldsDetail ? 'block' : 'hidden'
+                          } pb-10`}
+                        >
+                          <span className="text-xl text-[#657786]">
+                            {fieldItem.description}
+                          </span>
+                        </motion.div>
+                        <hr />
+                      </div>
+                    );
+                  })}
+                  {/* 
+                  <div className="w-full">
+                    <div className="flex justify-between items-center py-10">
+                      <span className="text-4xl">Product Designer (PD)</span>
+                      <div className="flex flex-row items-center space-x-4">
+                        <span className="text-xl text-[#657786]">디자이너</span>
+                        <span>아이콘</span>
+                      </div>
+                    </div>
+                    <div className="hidden pb-10">
+                      <span className="text-xl text-[#657786]">
+                        브랜드 아이덴티티 (BI), 사용자 인터페이스 및 경험
+                        (UI/UX) 디자인 등을 통해 프로젝트의 시각화 및 전반적인
+                        사용 경험을 디자인합니다.
+                      </span>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="w-full">
+                    <div className="flex justify-between items-center py-10">
+                      <span className="text-4xl">Front-end Developer (FE)</span>
+                      <div className="flex flex-row items-center space-x-4">
+                        <span className="text-xl text-[#657786]">
+                          CLIENT 개발자
+                        </span>
+                        <span>아이콘</span>
+                      </div>
+                    </div>
+                    <div className="hidden pb-10">
+                      <span className="text-xl text-[#657786]">
+                        프론트엔드 개발자로 서비스가 실제로 사용자에게 동작되고,
+                        SERVER와의 통신을 원활히 할 수 있도록 개발합니다.
+                      </span>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="w-full">
+                    <div className="flex justify-between items-center py-10">
+                      <span className="text-4xl">Back-end Developer (FE)</span>
+                      <div className="flex flex-row items-center space-x-4">
+                        <span className="text-xl text-[#657786]">
+                          SERVER 개발자
+                        </span>
+                        <span>아이콘</span>
+                      </div>
+                    </div>
+                    <div className="hidden pb-10">
+                      <span className="text-xl text-[#657786]">
+                        백엔드 개발자로 사용자가 요청하는 데이터를 저장하고
+                        처리하며 CLIENT와의 통신을 원활히 할 수 있도록
+                        개발합니다.
+                      </span>
+                    </div>
+                  </div> */}
+                  <hr />
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div className="flex w-full justify-between py-20">
+              <span className="text-5xl font-suit">FAQ</span>
+              <div className="flex flex-row space-x-36 font-pretend">
+                <div className="flex flex-col space-y-3">
+                  <span className="text-5xl">2/27 - 3/3</span>
+                  <span className="text-xl text-[#657786]">서류 접수</span>
+                </div>
+                <div className="flex flex-col space-y-3">
+                  <span className="text-5xl">3/5 - 3/7</span>
+                  <span className="text-xl text-[#657786]">면접 진행</span>
+                </div>
+                <div className="flex flex-col space-y-3">
+                  <span className="text-5xl">3/9</span>
+                  <span className="text-xl text-[#657786]">최종 결과</span>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div className="flex w-full justify-between py-20">
+              <span className="text-5xl font-suit">MUST DO</span>
+              <div className="flex flex-row space-x-36 font-pretend">
+                <div className="flex flex-col space-y-3">
+                  <span className="text-5xl">2/27 - 3/3</span>
+                  <span className="text-xl text-[#657786]">서류 접수</span>
+                </div>
+                <div className="flex flex-col space-y-3">
+                  <span className="text-5xl">3/5 - 3/7</span>
+                  <span className="text-xl text-[#657786]">면접 진행</span>
+                </div>
+                <div className="flex flex-col space-y-3">
+                  <span className="text-5xl">3/9</span>
+                  <span className="text-xl text-[#657786]">최종 결과</span>
+                </div>
+              </div>
+            </div>
+            <hr />
+          </div>
         </div>
       </div>
       {/* <div className="">
