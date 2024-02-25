664c60c0b3b350d99a8060b6936d7c8e697c467f
MyeongGyu <woomg427@yonsei.ac.kr>
2024-02-26 05:10:47 +0900

    [Fix] .env.local AWS name

--- src/app/(model)/actions.js
+++ src/app/(model)/actions.js
@@ -9,7 +9,7 @@ const generateFileName = (bytes = 32) =>
 const s3 = new S3Client({
   region: process.env.AWS_BUCKET_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
+    accessKeyId: process.env.AWS_BUCKET_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
