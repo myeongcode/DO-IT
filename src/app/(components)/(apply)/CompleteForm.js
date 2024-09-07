445f28f68ce39c99d704608969aaf0a03428a30e
myeongcode <woomg427@yonsei.ac.kr>
2024-09-07 23:11:01 +0900

    Fix: 지원서 Complete 컴포넌트 텍스트 수정

--- src/app/(components)/(apply)/CompleteForm.js
+++ src/app/(components)/(apply)/CompleteForm.js
@@ -35,8 +35,11 @@ export default function CompleteForm({ response }) {
               />
             </svg>
             <span className="text-xl">
-              지원서 제출 중입니다... 창을 닫지 말아주세요 *혹시라도 로딩이
-              끝나지 않고 계속 진행된다면, DO IT 이메일로 문의주시기 바랍니다.
+              지원서 제출 중입니다... 창을 닫지 말아주세요
+            </span>
+            <span className="text-red-400">
+              *혹시라도 로딩이 끝나지 않고 계속 진행된다면, DO IT 이메일로
+              문의주시기 바랍니다.
             </span>
           </div>
         )}
